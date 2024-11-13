fb7b5d7795a840c95104731e4aca914d924b1a7b
Alex Abatov <a.aliksant315@gmail.com>
2024-11-14 04:18:41 +0900

    Display popular movie function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,53 @@
 const state = {
     current: window.location.pathname
 }
+
+async function displayPopularMovies() {
+    const {results} = await fetchAPIData('movie/popular')
+    console.log(results);
+    
+    results.forEach(movie =>{
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = ` 
+          <a href="movie-details.html?id=${movie.id}">
+           ${movie.poster_path
+            ? ` <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />` : ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+           }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+              <p> Rating: ${(movie.vote_average)}</p>
+
+            </p>
+          </div>`;
+
+        document.querySelector('#popular-movies').appendChild(div)
+
+    })
+}
+
+async function fetchAPIData(endpoint) {
+    const API_KEY = '26dbb75b0378c33f9ad2a755116887a5'
+    const API_URL = 'https://api.themoviedb.org/3/'
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
+
+    const data = await response.json()
+
+    return data
+    
+}
 //hihglight active link
 function HighlightActiveLink(){
     const links = document.querySelectorAll('.nav-link')
@@ -15,7 +62,7 @@ function init(){
     switch (state.current) {
         case '/':
         case '/index.html':
-            
+            displayPopularMovies()
             console.log("Home");
             break;
         case '/movie-details.html':
@@ -35,3 +82,20 @@ function init(){
 }
 init()
 
+
+
+{/* <div class="card">
+<a href="movie-details.html?id=1">
+  <img
+    src="images/no-image.jpg"
+    class="card-img-top"
+    alt="Movie Title"
+  />
+</a>
+<div class="card-body">
+  <h5 class="card-title">Movie Title</h5>
+  <p class="card-text">
+    <small class="text-muted">Release: XX/XX/XXXX</small>
+  </p>
+</div>
+</div> */}
