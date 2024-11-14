55f51cabe849d85b810cbc5a5a1b2de3c3aa4a95
Alex Abatov <a.aliksant315@gmail.com>
2024-11-15 04:16:12 +0900

    Show tv-details page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 const state = {
     current: window.location.pathname
 }
-
 async function displayPopularMovies() {
     const {results} = await fetchAPIData('movie/popular')
-    console.log(results);
+    //console.log(results);
     
     results.forEach(movie =>{
         const div = document.createElement('div')
@@ -95,10 +94,13 @@ function showSpinner(){
 function hideSpinner(){
   document.querySelector('.spinner').classList.remove('show')
 }
+
 async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1]
   const movie = await fetchAPIData(`movie/${movieId}`)
-  console.log(movie)
+ 
+
+  displayBackgroundImage('movie', movie.backdrop_path);
   const div = document.createElement('div')
   div.innerHTML = `<div class="details-top">
             ${movie.poster_path
@@ -145,6 +147,76 @@ async function displayMovieDetails() {
   
   
 }
+
+//Displays TV details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for background image
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    show.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${show.name}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+      ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${
+      show.homepage
+    }" target="_blank" class="btn">Visit show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number of Episodes:</span> ${
+      show.number_of_episodes
+    }</li>
+    <li><span class="text-secondary">Last Episode To Air:</span> ${
+      show.last_episode_to_air.name
+    }</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${show.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(', ')}
+  </div>
+</div>
+  `;
+
+  document.querySelector('#show-details').appendChild(div);
+}
 //hihglight active link
 function HighlightActiveLink(){
     const links = document.querySelectorAll('.nav-link')
@@ -158,6 +230,28 @@ function HighlightActiveLink(){
 function addCommasToNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+}
+
 function init(){
     switch (state.current) {
         case '/':
@@ -174,6 +268,7 @@ function init(){
             console.log("Shows details");
             break
         case '/tv-details.html':
+            displayShowDetails()
             console.log("ShowsTV details");
             break
         case '/search.html':
