544cc75ce6015e3dfd2895dfee066fa6b3bf5124
Alex Abatov <a.aliksant315@gmail.com>
2024-11-14 18:54:58 +0900

    Details page

--- js/script.js
+++ js/script.js
@@ -27,7 +27,10 @@ async function displayPopularMovies() {
             <h5 class="card-title">${movie.title}</h5>
             <p class="card-text">
               <small class="text-muted">Release: ${movie.release_date}</small>
-              <p> Rating: ${(movie.vote_average)}</p>
+              <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}/ 10
+            </p>
 
             </p>
           </div>`;
@@ -92,6 +95,56 @@ function showSpinner(){
 function hideSpinner(){
   document.querySelector('.spinner').classList.remove('show')
 }
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1]
+  const movie = await fetchAPIData(`movie/${movieId}`)
+  console.log(movie)
+  const div = document.createElement('div')
+  div.innerHTML = `<div class="details-top">
+            ${movie.poster_path
+          ? ` <img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.name}"
+          />` : ` <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.name}"
+          />`
+         }
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}/ 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+             ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime}</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+           ${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(',  ')}
+        </div>`
+  
+  document.querySelector('#movie-details').appendChild(div)
+  
+  
+}
 //hihglight active link
 function HighlightActiveLink(){
     const links = document.querySelectorAll('.nav-link')
@@ -102,6 +155,9 @@ function HighlightActiveLink(){
         
     });
 }
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 function init(){
     switch (state.current) {
         case '/':
@@ -110,6 +166,7 @@ function init(){
             console.log("Home");
             break;
         case '/movie-details.html':
+            displayMovieDetails()
             console.log("Movie details");
             break
         case '/shows.html':
@@ -126,21 +183,3 @@ function init(){
     HighlightActiveLink()
 }
 init()
-
-
-
-{/* <div class="card">
-<a href="movie-details.html?id=1">
-  <img
-    src="images/no-image.jpg"
-    class="card-img-top"
-    alt="Movie Title"
-  />
-</a>
-<div class="card-body">
-  <h5 class="card-title">Movie Title</h5>
-  <p class="card-text">
-    <small class="text-muted">Release: XX/XX/XXXX</small>
-  </p>
-</div>
-</div> */}
